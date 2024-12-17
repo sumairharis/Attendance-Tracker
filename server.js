5add595ad76e879886c6bd91569c7b4db5c6d443
Haris Sumair <sumair.haris@gmail.com>
2024-12-17 23:25:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,17 @@ const server = http.createServer((req, res) => {
 
   if (req.method === 'GET' && req.url === '/') {
     // Serve homepage
-    
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        console.error("Error loading index.html:", err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
   }
    else if (req.method === 'GET' && req.url === '/driver') {
     const filePath = path.join(__dirname, 'driver.html');
